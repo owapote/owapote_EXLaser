4864fb5662e0d3a32c550960f8a642ddc5f6f3e8
owapote <135196453+owapote@users.noreply.github.com>
2025-09-13 23:39:43 +0900

    version update 2.1.0 => 2.1.1

--- java/com/owapote/bc_ex_laser/BCEXLaserCore.java
+++ java/com/owapote/bc_ex_laser/BCEXLaserCore.java
@@ -26,7 +26,7 @@ public class BCEXLaserCore
 {
     public static final String MODID = "bc_ex_laser";
     public static final String NAME = "BuildCraft EXLaser";
-    public static final String VERSION = "2.1.0";
+    public static final String VERSION = "2.1.1";
 
     @SidedProxy(clientSide = "com.owapote.bc_ex_laser.proxy.ClientProxy",
                 serverSide = "com.owapote.bc_ex_laser.proxy.CommonProxy")
